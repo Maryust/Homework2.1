d897829e1535d83eb5c31042c2b1692a82bb0e4c
Maryust <masyanya1101@yandex.ru>
2021-09-10 12:56:58 +0300

    camelCase added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int ticketPriceInKopecks = 1829_90;
         // Цена за билет выраженная в копейках
-        int mileprice = 20;
-        int mile = ticketPriceInKopecks / 100 / mileprice;
+        int milePrice = 20;
+        int mile = ticketPriceInKopecks / 100 / milePrice;
         System.out.print("Начислено миль:" + mile);
     }
 }
